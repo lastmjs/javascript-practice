24f0ca0ae5bbbc7e44bdab21347938e01276b362
Jordan Last <jordan.michael.last@gmail.com>
2018-09-14 03:57:15 -0600

    add basic ui, add question view element

--- elements/jp-group.js
+++ elements/jp-group.js
@@ -1,13 +1,20 @@
 import '@polymer/paper-card/paper-card.js';
+import '@polymer/paper-button/paper-button.js';
+import 'prendus-question-elements/prendus-view-question.ts';
 
 interface Monkey {
-    
+
 }
 
 class JPGroup extends HTMLElement {
 
     connectedCallback() {
         this.render();
+
+        this.querySelector('#prendus-view-question').question = {
+            assessML: '[input1]',
+            javaScript: 'answer = input1 == 5'
+        };
     }
 
     render() {
@@ -20,10 +27,17 @@ class JPGroup extends HTMLElement {
                 }
             </style>
 
-            <paper-card class="question-container">
-                <div class="card-content">
-                    ${this.title}
+            <paper-card class="question-container" heading="${this.title}">
+                <div class="card-actions">
+                    <div style="padding: 25px">
+                    <prendus-view-question id="prendus-view-question"></prendus-view-question>
+                    </div>
                 </div>
+
+                <div class="card-actions">
+                    <paper-button>Next question</paper-button>
+                </div>
+
             </paper-card>
         `;
     }
