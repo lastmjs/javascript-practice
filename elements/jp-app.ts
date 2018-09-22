f8d2fccd77a06937308f8c325b7c8e809cc74ee4
Jordan Last <jordan.michael.last@gmail.com>
2018-09-21 19:34:04 -0600

    factor out into appropriate elements

--- elements/jp-app.ts
+++ elements/jp-app.ts
@@ -1,58 +1,32 @@
+import {html, render} from 'lit-html';
+import './jp-concept-map';
+
 class JPApp extends HTMLElement {
     connectedCallback() {
         this.render();
     }
 
     render() {
-        this.innerHTML = `
+        render(html`
             <style>
                 body {
                     margin: 0;
                 }
 
-                .concepts-container {
-                    display: flex;
-                    flex-direction: column;
-                    text-align: center;
-                }
-
-                .concept {
-                    flex-grow: 1;
-                    padding: 1em;
-                    border: 1px solid black;
-                    cursor: pointer;
-                }
-
-                .concept:hover {
-                    background-color: rgba(1, 1, 1, .1);
+                .main-grid {
+                    display: grid;
+                    grid-template-columns: 20em 50vw;
                 }
             </style>
 
-            <div style="display: grid; grid-template-columns: 20em 50vw">
-                <div class="concepts-container">
-                    <div class="concept">Primitive data types</div>
-                    <div class="concept">Objects</div>
-                    <div class="concept">Functions</div>
-                    <div class="concept">Arrays</div>
-                    <div class="concept">Classes</div>
-                    <div class="concept">Modules</div>
-                    <div class="concept">Operators</div>
-                    <div class="concept">Control flow</div>
-                    <div class="concept">Variables</div>
-                    <div class="concept">Promises</div>
-                    <div class="concept">async/await</div>
-                    <div class="concept">Generators</div>
-                    <div class="concept">Scope</div>
-                    <div class="concept">Closures</div>
-                    <div class="concept">Callbacks</div>
-                    <div class="concept">Proxies</div>
-                </div>
+            <div class="main-grid">
+                <jp-concept-map></jp-concept-map>
 
                 <div>
-                    mlonkey
+                    monkey
                 </div>
             </div>
-        `;
+        `, this);
     }
 }
 
