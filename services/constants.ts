e9cee084d66dd328800733f738efc7ab2231d876
Jordan Last <jordan.michael.last@gmail.com>
2018-09-26 23:20:39 -0600

    add staging environment

--- services/constants.ts
+++ services/constants.ts
@@ -1,1 +1,5 @@
-export const GRAPHQL_HTTP_ENDPOINT = window.process.env.NODE_ENV === 'production' ? `https://us1.prisma.sh/jordan-last/javascript-practice/dev` : `http://localhost:4466`;
+export const GRAPHQL_HTTP_ENDPOINT = {
+    production: '',
+    staging: 'https://us1.prisma.sh/jordan-last/javascript-practice/dev',
+    development: 'http://localhost:4466'
+}[window.process.env.NODE_ENV];
