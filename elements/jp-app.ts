64ffc6b9f477bc2724631c4c01565c1ec9e31550
Jordan Last <jordan.michael.last@gmail.com>
2018-09-28 23:25:29 -0600

    add the top bar

--- elements/jp-app.ts
+++ elements/jp-app.ts
@@ -37,10 +37,6 @@ class JPApp extends HTMLElement {
                         height: 100%;
                         overflow: hidden;
                     }
-
-                    .menu-button {
-                        display: none;
-                    }
                 }
 
                 @media (max-width: 1024px) {
@@ -49,10 +45,6 @@ class JPApp extends HTMLElement {
                         grid-template-columns: 0% 100%;
                         height: 100%;
                         overflow: hidden;
-                    }
-
-                    .menu-button {
-                        display: block;
                     }
                 }
 
@@ -67,6 +59,19 @@ class JPApp extends HTMLElement {
                     height: ${window.innerHeight}px;
                     overflow: hidden;
                 }
+
+                .router-container {
+                    height: 90%;
+                }
+
+                .top-bar {
+                    height: 10%;
+                    border-left: 1px solid grey;
+                    display: flex;
+                    width: 100%;
+                    background-color: ${backgroundColor};
+                    box-shadow: 0px 4px 2px -2px grey;
+                }
             </style>
 
             <div class="main-grid">
@@ -76,13 +81,13 @@ class JPApp extends HTMLElement {
 
                     <jp-load-indicator .hide=${state.hideLoadIndicator} .lower=${state.lowerLoadIndicator}></jp-load-indicator>
                     
-                    <!-- <div style="border-left: 1px solid grey; display: flex; width: 100%; background-color: ${backgroundColor}; box-shadow: 0px 4px 2px -2px grey">
+                    <div class="top-bar">
                         <button id="main-menu-button" class="menu-button" @click=${() => this.mainMenuToggle()}>Menu</button>
-                        <button ?hidden=${state.currentAssessment && state.currentAssessment.order === 0} class="previous-question-button" @click=${(e: any) => this.previousQuestionClick()}>Previous question</button>
-                        <button ?hidden=${state.currentAssessment && state.currentConcept && state.currentAssessment.order === state.currentConcept.assessments.length - 1} class="next-question-button" @click=${(e: any) => this.nextQuestionClick(state)}>Next question</button>
-                    </div> -->
+                    </div>
 
-                    <jp-router></jp-router>
+                    <div class="router-container">
+                        <jp-router></jp-router>
+                    </div>
 
                 </div>
 
