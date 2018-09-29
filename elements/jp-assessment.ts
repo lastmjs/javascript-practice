03200a53da0945f16322a3fdfd53039eaefc87dd
Jordan Last <jordan.michael.last@gmail.com>
2018-09-29 14:58:26 -0600

    When you click next or previous question, that should reset the question/solution buttons

--- elements/jp-assessment.ts
+++ elements/jp-assessment.ts
@@ -50,7 +50,6 @@ class JPAssessment extends HTMLElement {
             if (
                 state.currentAssessment === this.previousAssessment &&
                 state.currentConcept === this.previousConcept
-            
             ) {
                 return;
             }
@@ -90,6 +89,9 @@ class JPAssessment extends HTMLElement {
         Store.dispatch({
             type: 'NEXT_QUESTION'
         });
+
+        this.querySelector('#solution-button').innerHTML = `Solution`;
+        this.querySelector('#submit-button').removeAttribute('disabled');
     }
 
     previousAssessmentClick() {
@@ -101,6 +103,19 @@ class JPAssessment extends HTMLElement {
     showSolution() {
         const prendusViewQuestion = this.querySelector('#prendus-view-question');
         prendusViewQuestion.showSolutionClick();
+
+        //TODO everything below here is evil
+        //TODO rendering of this component really needs to be redone
+        const solutionTemplate = <HTMLTemplateElement> this.querySelector('#solution1');
+
+        if (solutionTemplate) {
+            this.querySelector('#solution-button').innerHTML = `Solution`;
+            this.querySelector('#submit-button').removeAttribute('disabled');
+        }
+        else {
+            this.querySelector('#solution-button').innerHTML = `Question`;
+            this.querySelector('#submit-button').setAttribute('disabled', true);
+        }
     }
 
     submitAnswer() {
@@ -184,6 +199,7 @@ class JPAssessment extends HTMLElement {
                         </button>
 
                         <button
+                            id="solution-button"
                             class="bottom-button"
                             @click=${() => this.showSolution()}
                         >
@@ -191,6 +207,7 @@ class JPAssessment extends HTMLElement {
                         </button>
 
                         <button
+                            id="submit-button"
                             class="bottom-button"
                             @click=${() => this.submitAnswer()}
                         >
