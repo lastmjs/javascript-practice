f9163649e92d35ae91757decacc267836dda24ba
Jordan Last <jordan.michael.last@gmail.com>
2018-10-04 13:09:39 -0600

    add hamgurger menu, spruce up body click listener defense

---
+++ elements/jp-hamburger.ts
@@ -0,0 +1,35 @@
+import {html, render} from 'lit-html';
+import {Store} from '../services/store';
+
+class JPHamburger extends HTMLElement {
+    connectedCallback() {
+        Store.subscribe(() => render(this.render(Store.getState()), this));
+    }
+
+    render(state: any) {
+        return html`
+            <style>
+                .hamburger-container {
+                    font-size: calc(1px + 1vmin);
+                    cursor: pointer;
+                    text-align: center;
+                    vertical-align: middle;
+                }
+
+                .hamburger-row {
+                    height: .5em;
+                    width: 3.5em;
+                    background-color: black;
+                }
+            </style>
+
+            <div class="hamburger-container">
+                <div class="hamburger-row" style="margin-bottom: .5em"></div>
+                <div class="hamburger-row" style="margin-bottom: .5em"></div>
+                <div class="hamburger-row"></div>
+            </div>
+        `;
+    }
+}
+
+window.customElements.define('jp-hamburger', JPHamburger);
