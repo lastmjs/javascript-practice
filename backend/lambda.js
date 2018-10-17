7fba6beaa71492fd9f27119addf5e43c5bc238e7
Jordan Last <jordan.michael.last@gmail.com>
2018-10-17 12:28:28 -0600

    test

--- backend/lambda.js
+++ backend/lambda.js
@@ -2,57 +2,53 @@ import { GraphQLServerLambda } from 'graphql-yoga';
 import { Prisma } from 'prisma-binding';
 import { typeDefs } from './generated/prisma/prisma-schema.js';
 
-console.log(process.env);
+// http://localhost:4466
 
-// const prisma = new Prisma({
-//     typeDefs,
-//     endpoint: process.env.CONTEXT === 'production' || process.env.CONTEXT === 'deploy-preview' || process.env.CONTEXT === 'branch-deploy' ? 'https://us1.prisma.sh/jordan-last/javascript-practice/dev' : 'http://localhost:4466'
-// });
+const prisma = new Prisma({
+    typeDefs,
+    endpoint: process.env.CONTEXT === 'production' || process.env.CONTEXT === 'deploy-preview' || process.env.CONTEXT === 'branch-deploy' ? 'https://us1.prisma.sh/jordan-last/javascript-practice/dev' : 'https://us1.prisma.sh/jordan-last/javascript-practice/dev'
+});
 
-// const preparedTopLevelQueryResolvers = prepareTopLevelResolvers(prisma.query);
-// const preparedTopLevelMutationResolvers = prepareTopLevelResolvers(prisma.mutation);
+const preparedTopLevelQueryResolvers = prepareTopLevelResolvers(prisma.query);
+const preparedTopLevelMutationResolvers = prepareTopLevelResolvers(prisma.mutation);
 
-// const resolvers = {
-//     Query: {
-//         ...preparedTopLevelQueryResolvers
-//     },
-//     Mutation: {
-//         ...preparedTopLevelMutationResolvers
-//     }
-// };
+const resolvers = {
+    Query: {
+        ...preparedTopLevelQueryResolvers
+    },
+    Mutation: {
+        ...preparedTopLevelMutationResolvers
+    }
+};
 
 export const handler = async (event, context, callback) => {
     try {
-        callback(null, {
-            statusCode: 200,
-            body: JSON.stringify(process.env)
+        const server = new GraphQLServerLambda({
+            typeDefs,
+            resolvers
         });
-        // const server = new GraphQLServerLambda({
-        //     typeDefs,
-        //     resolvers
-        // });
 
-        // server.graphqlHandler(event, context, (error, output) => {
-        //     callback(error, {
-        //         ...output,
-        //         statusCode: 200
-        //     });
-        // });
+        server.graphqlHandler(event, context, (error, output) => {
+            callback(error, {
+                ...output,
+                statusCode: 200
+            });
+        });
     }
     catch(error) {
         console.log(error);
     }
 };
 
-// function prepareTopLevelResolvers(resolverObject) {
-//     return Object.entries(resolverObject).reduce((result, entry) => {
-//             const resolverName = entry[0];
-//             const resolverFunction = entry[1];
-//             return {
-//                 ...result,
-//                 [resolverName]: async (parent, args, context, info) => {
-//                     return await resolverFunction(args, info);
-//                 }
-//             };
-//     }, {});
-// }
+function prepareTopLevelResolvers(resolverObject) {
+    return Object.entries(resolverObject).reduce((result, entry) => {
+            const resolverName = entry[0];
+            const resolverFunction = entry[1];
+            return {
+                ...result,
+                [resolverName]: async (parent, args, context, info) => {
+                    return await resolverFunction(args, info);
+                }
+            };
+    }, {});
+}
