30d322e9346def0095079f8283fede77176a0ffa
Jordan Last <jordan.michael.last@gmail.com>
2018-10-23 17:54:28 -0600

    TokenTransaction locked down

--- backend/schema-directives/visibility-directive.js
+++ backend/schema-directives/visibility-directive.js
@@ -1,6 +1,7 @@
 import { SchemaDirectiveVisitor } from 'graphql-tools';
 import { defaultFieldResolver } from 'graphql';
 import { getUserId } from '../services/utilities.js';
+import { prisma } from '../lambda.js';
 
 export class VisibilityDirective extends SchemaDirectiveVisitor {
     visitFieldDefinition(field, details) {
@@ -10,6 +11,24 @@ export class VisibilityDirective extends SchemaDirectiveVisitor {
             if (this.args.type === 'OWNER') {
                 if (details.objectType.name === 'User') {
                     if (parent.id !== getUserId(context, 'Not authorized')) {
+                        throw new Error('Not authorized');
+                    }
+                }
+
+                if (details.objectType.name === 'TokenTransaction') {
+                    const tokenTransaction = await prisma.query.tokenTransaction({
+                        where: {
+                            id: parent.id
+                        }
+                    }, `
+                        {
+                            user {
+                                id
+                            }
+                        }
+                    `);
+
+                    if (tokenTransaction.user.id !== getUserId(context, 'Not authorized')) {
                         throw new Error('Not authorized');
                     }
                 }
